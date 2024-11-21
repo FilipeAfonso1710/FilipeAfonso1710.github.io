7d885b920693ce8a23f83142d429ee53153256ed
FilipeAfonso1710 <a22304588@alunos.ulht.pt>
2024-11-21 11:53:58 +0000

    Lab 9 (quase)

--- lab9/script.js
+++ lab9/script.js
@@ -1,10 +1,26 @@
 const main = document.querySelector('main');
+const aside = document.querySelector('aside');
+
+
+/*Verifica se já existe*/
+
+if (!localStorage.getItem('cesto')) {
+    localStorage.setItem('cesto', "[]");
+}
+    
+let cesto = JSON.parse(localStorage.getItem('cesto'));
+
+
+/*Só um teste*/
+console.log(cesto)
+
+
 
 //carrega a página e afins antes de executar a função
 document.addEventListener('DOMContentLoaded', function () {
     
     produtos.forEach(produto => {
-        const listaProdutos = document.createElement('section');
+        const listaProdutos = document.createElement('article');
         const titulo = document.createElement("p");
         const genero = document.createElement("p");
         const imagem = document.createElement("img");
@@ -15,9 +31,41 @@ document.addEventListener('DOMContentLoaded', function () {
         genero.textContent = produto.category;
         imagem.src = produto.image;
         descricao.textContent = produto.description;
-        botao.textContent= "Adicionar ao cesto"
+        botao.textContent= "+ Adicionar ao cesto"
+        
 
         listaProdutos.append(titulo,imagem,descricao,botao);
         main.append(listaProdutos);
+
+        /*Adiciona o produto ao cesto*/
+
+        botao.addEventListener('click',() => {
+          cesto.push(produto);
+          localStorage.setItem('cesto', JSON.stringify(cesto));
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function atualizaCesto(){
+    cesto.forEach(produto => {
+        const produtosNoCesto = document.createElement('article');
+        const titulo = document.createElement("p");
+        const genero = document.createElement("p");
+        const imagem = document.createElement("img");
+        const botao = document.createElement("button");
+
+        titulo.textContent = produto.title;
+        genero.textContent = produto.category;
+        imagem.src = produto.image;
+        botao.textContent= "Remover do cesto"
+
+        produtosNoCesto.append(titulo,imagem,botao)
+        aside.append(produtosNoCesto);
+
+        botao.addEventListener('click',() => {
+            const indice = frutas.indexOf("produto");
+            retirado = cesto.splice(indice,1);
+            localStorage.setItem('cesto', JSON.stringify(cesto));
+          });
     });
 });
